84affd93193299bc15cc15d7d8310979966b631c
kingsleyh <kingsley.xa.hendrickse@barclayscapital.comm>
2017-08-27 00:09:10 +0100

    tweaking accounts

--- src/Main.elm
+++ src/Main.elm
@@ -2,12 +2,12 @@ module Main exposing (..)
 
 
 import Html exposing (Html, text)
-import Neo exposing (generatePrivateKey, getWIFFromBinaryPrivateKey)
+import Neo exposing (generateBinaryPrivateKey, getWIFFromBinaryPrivateKey)
 
 main : Html a
 main =
     let
-     pk = Debug.log "priv key" generatePrivateKey
+     pk = Debug.log "priv key" generateBinaryPrivateKey
      wif = Debug.log "wif" (getWIFFromBinaryPrivateKey pk)
     in
       text "Woop"
